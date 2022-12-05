1b1d21d1c630c996978283de45b456869d2556e9
Cleber Pereira da Silva <clebinho_logistica@hotmail.com>
2022-12-05 19:59:38 -0300

    Fisrt Commit

---
+++ Burger/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar/>
+  <Footer/>
+  <router-view/>
+</template>
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components:{
+    Navbar,
+    Footer
+    
+  }
+}
+</script>
+<style>
+
+
+</style>
---
+++ Burger/src/views/Pedidos.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="pedidos">
+        <h1>pedidos</h1>
+    </div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
